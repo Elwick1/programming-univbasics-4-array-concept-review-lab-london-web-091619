52af1df9305652a353c0cd5e72336f62a829aacf
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 09:50:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,21 +18,6 @@ def find_max_value(array)
   max
 end
 
-
-#def find_min_value(array)
- #min = 0
-  #  array.length.times do |count|
-   #   if count == 0
-    #min = array[count]
-     # else
-      #  if array[count] < min
-       #   min = array[count]
-    #    end
-     # end
-      #min
-  #  end
-  #end
-  
   def find_min_value(array)
   min = 0
     array.length.times do |count|
